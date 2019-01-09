b5f35a34a3a3aa67029ef3a66dedb95db1ce41c1
tawachan <tawachan@pixiv.com>
2019-01-10 00:24:30 +0900

    顔切り抜きスクリプト刷新

---
+++ crop.py
@@ -0,0 +1,33 @@
+import cv2
+
+cascade_path = "./haarcascade_frontalface_alt.xml"
+origin_image_path = "./images/"
+dir_path = "./cropped/"
+
+i = 0
+
+for line in open('./images/index.txt','r'):
+    line = line.rstrip()
+    print(line)
+    image = cv2.imread(origin_image_path+line, cv2.IMREAD_COLOR)
+    if image is None:
+        print('Not open : ',line)
+        continue
+
+    cascade = cv2.CascadeClassifier(cascade_path)
+    facerect = cascade.detectMultiScale(image, scaleFactor=1.8, minNeighbors=1, minSize=(200, 200))
+
+    if len(facerect) > 0:
+        for rect in facerect:
+            
+            x = rect[0]
+            y = rect[1]
+            width = rect[2]
+            height = rect[3]
+            dst = image[y:y + height, x:x + width]
+            save_path = dir_path + '/' + 'image(' + str(i) + ')' + '.jpg'
+            
+            cv2.imwrite(save_path, dst)
+            print("save!")
+            i += 1
+print("Finish")
